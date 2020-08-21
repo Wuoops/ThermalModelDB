fc789708461ae96a013cd61ed1b7660ddab41cb0
wu.quentin <wu.quentin@inventec.com.cn>
2020-08-21 09:52:43 +0800

    a

--- modelDb/config.py
+++ modelDb/config.py
@@ -2,5 +2,5 @@ class Config():
     # 直接用一个Py文件作为配置文件是不是很酷？
     ftpPath = '/var/ftp/'
     ftpdAddr = 'ftp://localhost/'
-    lines = 10
+    lines = 20
     picAddr = 'http://localhost:3333/'
---
+++ modelDb/venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/qun/Desktop/Project/ModelDB/modelDb/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
